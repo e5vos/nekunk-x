a12882c4b819f339ba7efdf21b5e1a52dc00eded
Barnabás Gőz <barnagoz@icloud.com>
2022-12-19 19:42:49 +0100

    feat: add web monitoring badge

--- apps/web/components/footer.jsx
+++ apps/web/components/footer.jsx
@@ -3,16 +3,25 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="w-full h-auto bg-blue-900 bg-opacity-40 text-center p-4">
-      <p className=" font-poppins font-semibold">
-        Minden jog fenntartva © {new Date().getFullYear()} - Nekünk X
-      </p>
-      <p className="text-sm font-poppins font-normal">
-        Developed by{" "}
-        <Link className="underline" href="https://github.com/barnagoz">
-          Barnabás Gőz
-        </Link>
-      </p>
+    <div className="w-full h-auto bg-blue-900 bg-opacity-40 text-center p-4 flex gap-4 justify-center">
+      <a target="_blank" href="https://betteruptime.com/">
+        <img
+          style={{ width: "130px", height: "52px" }}
+          alt="Better Uptime Website Monitoring"
+          src="https://betteruptime.com/assets/static_assets/badges/dark.png"
+        />
+      </a>
+      <div className="flex flex-col">
+        <p className=" font-poppins font-semibold">
+          Minden jog fenntartva © {new Date().getFullYear()} - Nekünk X
+        </p>
+        <p className="text-sm font-poppins font-normal">
+          Developed by{" "}
+          <Link className="underline" href="https://github.com/barnagoz">
+            Barnabás Gőz
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
